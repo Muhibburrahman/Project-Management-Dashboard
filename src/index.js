509d0a2e7f19f5e01f00cd560a3af5f42f68d0d0
adititiwariiiii <73654601+adititiwariiiii@users.noreply.github.com>
2022-09-08 21:32:43 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+=======
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+>>>>>>> 3aa10852c63aac46aee750cead8b8f9e550b6b0f
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,10 @@
+import "./list.scss";
+
+
+const List = () => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List;
