819e9c8e26d1ddfac2e5f4fec5116862f951cbea
adititiwariiiii <73654601+adititiwariiiii@users.noreply.github.com>
2022-09-09 21:08:24 +0530

    Revert "Add files via upload"

--- src/pages/new/New.jsx
+++
@@ -1,9 +0,0 @@
-import "./new.scss";
-
-const New = () => {
-  return (
-    <div>New</div>
-  )
-}
-
-export default New;
--- src/pages/single/Single.jsx
+++
@@ -1,9 +0,0 @@
-import "./single.scss";
-
-const Single = () => {
-  return (
-    <div>Single</div>
-  )
-}
-
-export default Single;
